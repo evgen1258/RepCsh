94d9ec6e20d97a750379dd8346669d6b652102d2
evgen1258 <dzha.evgen@mail.ru>
2015-11-13 13:58:45 +0600

    Урок3 классы

--- Sneke/Sneke/Program.cs
+++ Sneke/Sneke/Program.cs
@@ -10,14 +10,24 @@ namespace Sneke
     {
         static void Main(string[] args)
         {
+            Point p1 = new Point();
+            p1.x = 1;
+            p1.y=2;
+            p1.sym = '&';
+            Proba(p1.x, p1.y, p1.sym);
             
-            int x1=1;
+           /* int x1=1;
             int y1 = 3;
             char sym1 = '*';
             Proba(x1, y1, sym1);
+           
+            */
+           
             int x2 = 2;
             int y2 = 5;
             char sym2 = '#';
+            
+            
             Proba(x2, y2, sym2);
 
             
